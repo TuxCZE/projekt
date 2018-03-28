98d70aba079ce8d505afb089911f01529cd8c2a0
TuxCZE <37384836+TuxCZE@users.noreply.github.com>
2018-03-28 11:10:07 +0200

    Služby a destinace

--- resources/views/destinace.blade.php
+++ resources/views/destinace.blade.php
@@ -1,7 +1,122 @@
 @extends('rozvrzeni.stranka')
 @section('content')
-  @include('rozvrzeni.banner')
-
-  text
+  
+  <!-- banner-bottom -->
+	<div class="banner-bottom">
+		<!-- container -->
+		<div class="container">
+			<div class="banner-bottom-info">
+				<h3>Destinace</h3>
+			</div>
+			<div class="banner-bottom-grids">
+				<div class="col-md-4 banner-bottom-grid">
+					<div class="top-destinations-grids">
+						<div class="top-destinations-info">
+							<h4>Náhodné nabídky</h4>
+						</div>
+            
+						<div class="top-destinations-bottom">
+							<div class="td-grids">
+								<div class="col-xs-3 td-left">
+									<img src="images/t1.jpg" alt="">
+								</div>
+								<div class="col-xs-7 td-middle">
+									<a href="single.html">Donec libero id lacinia</a>
+									<p>Dapibus eu purus vel libero in nunc</p>
+									<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</div>
+								<div class="col-xs-2 td-right">
+									<p>$190</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="td-grids">
+								<div class="col-xs-3 td-left">
+									<img src="images/t2.jpg" alt="">
+								</div>
+								<div class="col-xs-7 td-middle">
+									<a href="single.html">Donec libero id lacinia</a>
+									<p>Dapibus eu purus vel libero in nunc</p>
+									<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</div>
+								<div class="col-xs-2 td-right">
+									<p>$213</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="td-grids">
+								<div class="col-xs-3 td-left">
+									<img src="images/t3.jpg" alt="">
+								</div>
+								<div class="col-xs-7 td-middle">
+									<a href="single.html">Donec libero id lacinia</a>
+									<p>Dapibus eu purus vel libero in nunc</p>
+									<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</div>
+								<div class="col-xs-2 td-right">
+									<p>$176</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="td-grids">
+								<div class="col-xs-3 td-left">
+									<img src="images/t4.jpg" alt="">
+								</div>
+								<div class="col-xs-7 td-middle">
+									<a href="single.html">Donec libero id lacinia</a>
+									<p>Dapibus eu purus vel libero in nunc</p>
+									<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</div>
+								<div class="col-xs-2 td-right">
+									<p>$490</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+					</div>
+				</div>
+        
+				<div class="col-md-8 banner-bottom-grid holidays-bottom-grid">
+					<div class="holidays-grids">
+						<div class="holidays-info">
+							<h4>Místa, kam se s námi můžete podívat</h4>
+						</div>
+            
+						<div class="holidays-grid">
+							<a href="products.html"><img src="images/h5.jpg" alt="" /></a>
+						</div>
+					</div>
+          
+					<div class="holidays-top-grids">
+						<div class="col-md-6 holidays-top-grid">
+							<a href="products.html"><img src="images/h2.jpg" alt="" /></a>
+						</div>
+            
+						<div class="col-md-6 holidays-top-grid">
+							<a href="products.html"><img src="images/a4.jpg" alt="" /></a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+          
+					<div class="holidays-top-grids">
+						<div class="col-md-6 holidays-top-grid">
+							<a href="products.html"><img src="images/a2.jpg" alt="" /></a>
+						</div>
+						<div class="col-md-6 holidays-top-grid">
+							<a href="products.html"><img src="images/h1.jpg" alt="" /></a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+          
+					<div class="holidays-grid">
+						<a href="products.html"><img src="images/h5.jpg" alt="" /></a>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner-bottom -->
   
 @endsection
--- resources/views/sluzby.blade.php
+++ resources/views/sluzby.blade.php
@@ -1,7 +1,46 @@
 @extends('rozvrzeni.stranka')
 @section('content')
-  @include('rozvrzeni.banner')
+ 
+  <!-- banner-bottom -->
+	<div class="banner-bottom">
+		<!-- container -->
+		<div class="container">
+			<div class="about-info">
+				<h2>Služby</h2>
+			</div>
+      
+			<div class="faqs-top-grids terms-grids">	
+				<h5>Zajistěte si už před odjezdem dovolenou bez starostí!</h5>																
+				<p>eště než začnete přemýšlet, co s sebou do kufru, nezapomeňte si položit otázku, zda vám nechybí pojištění nebo jak se pohodlně dopravit na letiště. Zvažte také, kolik věcí s sebou na dovolenou povezete a jestli nebude vhodé dopředu si zajistit nadváhu zavazadel, abyste později na letišti nemuseli platit zbytečně vysokou částku.</p>
+        <p>&nbsp;</p>
 
-  text
+        <h5>Exkluzivní cestovní pojištění zájezdů</h5>			
+        <p>Již při rezervaci zájezdu Vám doporučujeme sjednat si komplexní pojištění od ERV Evropské pojišťovny.</p>
+        <p>&nbsp;</p>
+        
+        <h5>Transfery na letiště v Praze</h5>			
+        <p>Zajistíme vám pohodlný transfer z jakéhokoliv místa v Praze až na letiště Václava Havla Praha.</p>
+        <p>&nbsp;</p>
+        
+        <h5>Nadváha zavazadel</h5>			
+        <p>Víte, že bude vaše zavazadlo vážit víc, než je povolený limit? Objednejte si nadváhu zavazadla za výhodnější cenu, než kterou byste platili na letišti při odbavení.</p>
+        <p>Na charterových letech ve třídě Economy je maximální povolená váha zavazadla 15 kilogramů. U odbavení se za každý kilogram nadváhy navíc počítá poplatek 160 Kč. S námi si můžete objednat přepravu zavazadel nad tento váhový limit za mnohem lepší ceny. Nechte starosti se zavazadly na nás!</p>
+        <p>&nbsp;</p>
+        
+        <h5>Dopřejte si i v letadle nadstandardní služby</h5>			
+        <p>Nenechte náhodě, aby se stalo, že v letadle nebudete sedět se svými nejbližšími vedle sebe. Rezervujte si dopředu svá místa!</p>
+        <p>Také stravu v letadle můžete na vybraných letech ovlivnit. Dopřejte na palubě bohatší menu. A pokud si chcete dopřát kompletní VIP servis, neváhejte využít službu Travel Plus Comfort.</p>
+        <p>&nbsp;</p>
+        
+				<div class="terms-bottom-grids">
+					<h6>Nebojte se, říct nám o svých speciálních požadavcích!</h6>
+					<p>Trápí vás omezení v přepravě, či stravě? Vynasnažíme se pro vás zařídit vše pro vaši maximální spokojenost.</p>
+          <p>Dopředu jsme také schopni vyhovět vám v případě vašich přání ohledně cestování s dětmi do dvou let, přepravy zvířat i sportovního vybavení.</p>
+				</div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner-bottom -->
   
 @endsection
